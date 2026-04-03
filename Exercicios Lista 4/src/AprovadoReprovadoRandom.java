8a8cfc6b15bb863ee133b268770def7cbeb51d00
vito <vitor.cynh@gmail.com>
2026-04-03 16:20:05 -0300

    feature(exercise) lista 4

---
+++ Exercicios Lista 4/src/AprovadoReprovadoRandom.java
@@ -0,0 +1,8 @@
+public class AprovadoReprovadoRandom {
+    public static void main(String[] args) {
+        int numRandom = (int)(Math.random() * 10 + 1);
+        System.out.println(numRandom);
+    }
+}
+
+// Victor Hugo dos Santos
