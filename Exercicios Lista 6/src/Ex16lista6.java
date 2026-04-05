92fa54dc59269392edaf014b1396d3aa9e600576
vito <vitor.cynh@gmail.com>
2026-04-05 12:10:05 -0300

    feature(exercise) lista 6

---
+++ Exercicios Lista 6/src/Ex16lista6.java
@@ -0,0 +1,16 @@
+public class Ex16lista6 {
+    public static void main(String[] args) {
+
+        int soma;
+        double media;
+        soma = 0;
+        for (int i = 15; i <= 100; i++){
+            soma += i;
+
+        }
+        media = (double)soma/86;
+        System.out.println("A media é: " + media);
+    }
+}
+
+// -- Victor Hugo dos Santos -- //
