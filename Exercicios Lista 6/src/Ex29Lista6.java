c50f1ffd3896c21f10330a5135a567a26ded491e
vito <vitor.cynh@gmail.com>
2026-04-05 19:05:15 -0300

    feature(exercise) lista 6

---
+++ Exercicios Lista 6/src/Ex29Lista6.java
@@ -0,0 +1,19 @@
+public class Ex29Lista6 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + ", ");
+
+            for (int j = 1; j <= 5; j++) {
+                if (i % 2 != 0) {
+                    System.out.print((2 * j - 1) + " ");
+                } else {
+                    System.out.print((2 * j) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// -- Victor Hugo dos Santos -- //
---
+++ Exercicios Lista 6/src/Ex31Lista6.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ex31Lista6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int quantidade;
+
+        do {
+            System.out.print("Entre com a quantidade de termos (0 para sair): ");
+            quantidade = sc.nextInt();
+
+            if (quantidade < 0) {
+                System.out.println("Numero invalido. Tente novamente.");
+            } else if (quantidade > 0) {
+                for (int i = 1; i <= quantidade; i++) {
+                    System.out.print(i + " ");
+                }
+                System.out.println("\n");
+            }
+        } while (quantidade != 0);
+
+        System.out.println("Fim do programa");
+    }
+}
+
+// -- Victor Hugo dos Santos -- //
