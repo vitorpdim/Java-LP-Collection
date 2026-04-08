afe2bc38a16ec5e7de807572d02b6ef4d806b200
vito <vitor.cynh@gmail.com>
2026-04-08 20:35:40 -0300

    feature(exercise) lista 6

---
+++ Exercicios Lista 6/src/Ex53lista6.java
@@ -0,0 +1,16 @@
+public class Ex53lista6 {
+    public static void main(String[] args) {
+
+        int a = 0, b = 1, c = a + b;
+        while(a  <= 250){
+
+            System.out.print(a + " ");
+            a = b;
+            b = c;
+            c = a + b;
+
+        }
+    }
+}
+
+// -- Victor Hugo dos Santos -- //
